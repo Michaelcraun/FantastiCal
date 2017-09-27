cf1f2f82fbb17d64bddfe220bcb8e1d08ab2b457
Michael Craun <michaelcraun@michaels-macbook-air.local>
2017-09-27 09:49:49 -0400

    Initial Commit.

---
+++ Controller/Main/MainLayout.swift
@@ -0,0 +1,17 @@
+//
+//  MainLayout.swift
+//  FantastiCal
+//
+//  Created by Michael Craun on 9/27/17.
+//  Copyright © 2017 Craunic Productions. All rights reserved.
+//
+
+import Foundation
+
+extension MainVC {
+    
+    func layoutView() {
+        
+        
+    }
+}
---
+++ Controller/Main/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FantastiCal
+//
+//  Created by Michael Craun on 9/27/17.
+//  Copyright © 2017 Craunic Productions. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    var monthCollection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layoutView()
+        
+    }
+}
